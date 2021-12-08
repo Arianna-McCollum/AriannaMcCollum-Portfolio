9eade755e58bb5d7cf112933a70c7992de23c21d
Electrolion <AMccollum71@gmail.com>
2021-12-08 15:59:24 -0500

    updated resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from "../../assets/AriannaMcCollumResume.pdf";
+import resume from "../../assets/AriannaMcCollumResume.docx";
 
 function About() {
   return (
